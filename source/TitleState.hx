effb404320cfe084964aa1806535b1aba51bf5c3
Lily <egzozu.be.bas@gmail.com>
2025-02-16 06:02:22 +0300

    no more dc

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -108,7 +108,7 @@ class TitleState extends MusicBeatState
 		if (!FlxG.save.data.did_warning)
 		{
 			FlxG.save.data.did_warning = true;
-			CoolUtil.showPopUp("Please note that Jordan Santiago does not work on this port!\n\nIf you have any issues or questions with this version, please contact @mcagabe19 (mc.9 on Discord)!\n\n", "Notice!");
+			CoolUtil.showPopUp("Please note that Jordan Santiago does not work on this port!\n\nIf you have any issues or questions with this version, please contact @mcagabe19!\n\n", "Notice!");
 		}
 
 		#if (CHECK_FOR_UPDATES)
