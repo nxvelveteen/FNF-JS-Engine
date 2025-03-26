163e430c0e7eafa8a9432f2a081f5b6f16b9c33c
Stinko <145304742+StinkTheStinker@users.noreply.github.com>
2025-03-26 16:18:39 -0400

    yeah

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -99,7 +99,7 @@ class ChromaticAberrationEffect extends Effect
 
 }
 
-
+/*
 class ScanlineEffect extends Effect
 {
 	
@@ -111,7 +111,7 @@ class ScanlineEffect extends Effect
 	
 	
 }
-
+*/
 
 class Scanline extends FlxShader
 {
@@ -447,7 +447,7 @@ class Grain extends FlxShader
 	
 	
 }
-
+/* // can these commented out shaders get fixed sometime?
 class VCRDistortionEffect extends Effect
 {
   public var shader:VCRDistortionShader = new VCRDistortionShader();
@@ -623,7 +623,7 @@ class VCRDistortionShader extends FlxShader // https://www.shadertoy.com/view/ld
     super();
   }
 }
-
+*/
 
 /*class VHSFilterAccurate extends FlxShader {
 	@:glFragmentSource('// Automatically converted with https://github.com/TheLeerName/ShadertoyToFlixel
